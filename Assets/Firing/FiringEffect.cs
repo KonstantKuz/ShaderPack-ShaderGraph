b2e1f834329d26083a558276378d9dd981ca399d
Konstantin Kuz <stalkercatkuz@gmail.com>
2020-08-07 22:16:36 +0300

    added firing shader

---
+++ Assets/Firing/FiringEffect.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FiringEffect : MonoBehaviour
+{
+    [SerializeField] private float firingProgressFrom;
+    [SerializeField] private float firingProgressTo;
+    [SerializeField] private float firingTime;
+    [SerializeField] private ParticleSystem fireEmbers;
+    [SerializeField] private MeshRenderer renderer;
+    
+    [ContextMenu("Fire")]
+    public void Fire()
+    {
+        fireEmbers.Play();
+        StartCoroutine(Fire());
+        IEnumerator Fire()
+        {
+            yield return new WaitForSeconds(Random.Range(0.5f, 1f));
+            float effectTime = 0;
+            while (effectTime < firingTime)
+            {
+                yield return new WaitForEndOfFrame();
+                effectTime += Time.deltaTime;
+                float dissolveProgress = Mathf.SmoothStep(firingProgressFrom, firingProgressTo, effectTime/firingTime);
+                renderer.material.SetFloat("Vector1_E09EAC5D",dissolveProgress);
+                fireEmbers.transform.localPosition = Vector3.Lerp(Vector3.up, -Vector3.up, effectTime/firingTime);
+            }
+        }
+    }
+
+    [ContextMenu("ResetEffect")]
+    public void ResetEffect()
+    {
+        fireEmbers.transform.localPosition = Vector3.up;
+        renderer.material.SetFloat("Vector1_E09EAC5D",firingProgressFrom);
+    }
+}
---
+++ Assets/Parallax/README.md
@@ -0,0 +1,7 @@
+# Parallax-Mapping-in-Unity-Shader-Graph
+
+Node code used in the [Tutorial](https://youtu.be/LKhGqKYOmbo)
+
+[My Material Pack](https://www.assetstore.unity3d.com/#!/content/136692)
+
+![Screenshot](http://anaseinea.github.io/Parallax-Mapping-Node-Unity/Preview_image.gif)
